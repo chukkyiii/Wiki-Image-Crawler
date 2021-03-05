c596b491837a1cee9e3f24c0f592f8f37216d94a
chuks <jgodwin42@icloud.com>
2021-03-05 00:08:03 +0000

    Flake8 corrections and slef creating imagearchive folder

--- wikiwalking.py
+++ wikiwalking.py
@@ -1,18 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib
-import time
+# import time
 import keyarchivemanager as kam
+import os
 
+os.mkdir("imagearchive")
 kn = kam.kam
-class wikifinder:
 
+
+class wikifinder:
     def wikiimagedownload(image_url, key):
         filepath = "./imagearchive/"
         filename = kn.knm(key) + "w" + ".jpg"
         full_path = filepath + filename
-        imgreq = urllib.request.urlretrieve(image_url, full_path)
-
+        return urllib.request.urlretrieve(image_url, full_path)
 
     def wikiwalk():
         url = 'https://en.wikipedia.org/wiki/Special:Random'
@@ -20,7 +22,7 @@ class wikifinder:
         while True:
             try:
                 wiki = requests.get(url)
-                #print(wiki.status_code)# --> just to show website status_code
+                # print(wiki.status_code)# --> just to show website status_code
 
                 soup = BeautifulSoup(wiki.content, 'html.parser')
                 imglink = soup.find('img', class_="thumbimage")
@@ -29,7 +31,7 @@ class wikifinder:
                 image_url = 'https:' + image_url
                 print("Image Found")
                 key += 1
-                #print(image_url) #--> just to show image url
+                # print(image_url) #--> just to show image url
                 wikifinder.wikiimagedownload(image_url, key)
 
             except AttributeError:
